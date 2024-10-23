686e032584a7518d61392d5cb514804cbb9b7cb8
HadjMohamed <hadj.tmohamed@gmail.com>
2024-10-23 22:16:55 +0200

    Changing app.py port

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def chatbot():
     return jsonify({"response": response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
